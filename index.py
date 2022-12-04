3ec5f69880bbdf0fba28bfe7ff61e7745767c274
adeonita <adeonita.sousa@gmail.com>
2022-12-04 16:20:22 -0300

    update index

--- index.py
+++ index.py
@@ -2,5 +2,7 @@ from btree import Btree
 
 file_path = "./files/student.csv"
 btree = Btree()
-data_frame = btree.create_data_frame(file_path)
-print(data_frame)
+# data_frame = btree.create_data_frame(file_path)
+# print(data_frame)
+
+error = print(btree.create(0))
